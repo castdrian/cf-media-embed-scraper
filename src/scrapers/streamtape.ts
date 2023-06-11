0dc603f34624642f0fdc821d81ba657e6f998ff7
adrifcastr <adrifcastr@gmail.com>
2023-06-11 17:47:47 +0200

    implement streamtape scraper

--- src/scrapers/streamtape.ts
+++ src/scrapers/streamtape.ts
@@ -2,12 +2,20 @@ import puppeteer from "@cloudflare/puppeteer";
 
 export default {
 	type: "streamtape",
-	async fetchMedia(url: string, env: any): Promise<string> {
+	async fetchMedia(url: string, env: any): Promise<string|undefined> {
 		const browser = await puppeteer.launch(env.MYBROWSER);
 		const page = await browser.newPage();
-		await page.goto(url);
-		await browser.close();
+		await page.goto(url, { waitUntil: "domcontentloaded" });
 
-		return url; // TODO: Implement scraper
+		const mainVideo = await page.$("#mainvideo");
+		const mainVideoSrc = await mainVideo?.evaluate((node) => node.getAttribute("src"));
+		await browser.close();
+		
+		if (!mainVideoSrc) {
+			return undefined;
+		}
+
+		const videoURL = `https:${mainVideoSrc}`;
+		return videoURL;
 	}
 }
